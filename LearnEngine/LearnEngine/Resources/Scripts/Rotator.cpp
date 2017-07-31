db5c60933024bd441e0b5d1069856324417bf394
Danylo Rudenko <danilyo97physics@gmail.com>
2017-07-31 20:04:03 +0300

    Intermediate testing.
    Signed-off-by: Danylo Rudenko <danilyo97physics@gmail.com>

--- LearnEngine/LearnEngine/Resources/Scripts/Rotator.cpp
+++ LearnEngine/LearnEngine/Resources/Scripts/Rotator.cpp
@@ -12,7 +12,7 @@ Rotator::Rotator() :
 void Rotator::OnRegistered()
 {
     target_ = owner_->GetComponent<GLObject>();
-    target_->SetScale(glm::vec3(2.f));
+    target_->SetScale(glm::vec3(0.3f));
     target_->SetPosition(glm::vec3(0.0f, -0.5f, 3.0f));
     SetTicking(true);
 }
@@ -20,24 +20,32 @@ void Rotator::OnRegistered()
 
 void Rotator::Tick()
 {
-    //target_->SetRotation(y_type(), y_rotation_);
-    y_rotation_ += 0.01f;
+    target_->SetRotation(y_type(), y_rotation_);
+    y_rotation_ += 1.1f;
     
-    auto old_pos = target_->GetRotation();
+    auto old_pos = target_->GetPosition();
     if (InputSystem::Instance().GetKey(GLFW_KEY_A)) {
-        target_->SetRotation(x_type(), old_pos.x + 1.01f);
+        target_->SetPosition(x_type(), old_pos.x + 0.01f);
     }
 
     if (InputSystem::Instance().GetKey(GLFW_KEY_D)) {
-        target_->SetRotation(x_type(), old_pos.x - 1.01f);
+        target_->SetPosition(x_type(), old_pos.x - 0.01f);
     }
 
     if (InputSystem::Instance().GetKey(GLFW_KEY_W)) {
-        target_->SetRotation(y_type(), old_pos.y + 1.01f);
+        target_->SetPosition(z_type(), old_pos.z + 0.01f);
     }
 
     if (InputSystem::Instance().GetKey(GLFW_KEY_S)) {
-        target_->SetRotation(y_type(), old_pos.y - 1.01f);
+        target_->SetPosition(z_type(), old_pos.z - 0.01f);
+    }
+
+    if (InputSystem::Instance().GetKey(GLFW_KEY_SPACE)) {
+        target_->SetPosition(y_type(), old_pos.y + 0.01f);
+    }
+
+    if (InputSystem::Instance().GetKey(GLFW_KEY_LEFT_CONTROL)) {
+        target_->SetPosition(y_type(), old_pos.y - 0.01f);
     }
 
     auto new_pos = target_->GetPosition();
